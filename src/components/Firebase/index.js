42ef7de8152a59c81f0d56676e222f5b129b215d
markeu <ucheuzochukwumark@gmail.com>
2020-11-17 09:56:03 +0100

    first commit

---
+++ src/components/Firebase/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Firebase() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+    )
+}
+
+export default Firebase
---
+++ src/components/Landing/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  )
+}
+
+export default Landing
