7c72ac36a235d9a5f0c7664d0863713613313cbe
markeu <ucheuzochukwumark@gmail.com>
2020-11-17 10:26:03 +0100

    set up default app routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-    )
-}
+import Navigation from '../Navigation';
+import LandingPage from '../Landing'
+import SignUpPage from '../SignUp'
+import SignInPage from '../SignIn'
+import PasswordForget from '../PasswordForget'
+import Home from '../Home'
+import AccountPage from '../Account'
+import AdminPage from '../Admin'
+import * as Routes from '../../constants/routes'
 
-export default App
+const {SIGN_IN, HOME, ACCOUNT, ADMIN, LANDING, SIGN_UP, PASSWORD_FORGET} = Routes
+
+const App = () => (
+  <Router>
+    <Navigation />
+    <hr />
+
+    <Route exact path={LANDING} component={LandingPage} />
+    <Route path={SIGN_UP} component={SignUpPage} />
+    <Route path={SIGN_IN} component={SignInPage} />
+    <Route path={PASSWORD_FORGET} component={PasswordForget} />
+    <Route path={HOME} component={Home} />
+    <Route path={ACCOUNT} component={AccountPage} />
+    <Route path={ADMIN} component={AdminPage}/>
+  </Router>
+);
+
+export default App;
